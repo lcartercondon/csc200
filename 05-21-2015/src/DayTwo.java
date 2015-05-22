19fab8bfbf0ade8f83b3cd300d310d103625b2c4
lcartercondon <liam.carter.condon@gmail.com>
2015-05-21 20:33:32 -0400

    simple implementation of an integer greedy algorithm while professor talked about scanners and tokens

--- 05-21-2015/src/DayTwo.java
+++ 05-21-2015/src/DayTwo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -14,7 +16,28 @@ public class DayTwo {
 	 * Talking about basic types, casting, basic operators
 	 */
 	public static void main(String[] args) {
+		Integer change = 99;
+		ArrayList<Integer> coins = new ArrayList<Integer>();
+		coins.add(25);
+		coins.add(10);
+		coins.add(5);
+		coins.add(1);
 		
+		ArrayList<Integer> handover = greedy(change, coins);
+		System.out.println("Change for " + change + " cents is " 
+							+ handover.get(0) + " quarters, " 
+							+ handover.get(1) + " dimes, " 
+							+ handover.get(2) + " nickels, and "
+							+ handover.get(3) + " pennies."
+							);		
 	}
 
+	private static ArrayList<Integer> greedy(Integer a, ArrayList<Integer> b){
+		ArrayList<Integer> retval = new ArrayList<Integer>();
+		for(int i = 0; i < b.size(); i++){
+			retval.add(a/b.get(i));
+			a %= b.get(i);
+		}
+		return retval;
+	}
 }
