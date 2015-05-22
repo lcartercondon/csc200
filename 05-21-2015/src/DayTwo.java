e098ce47ea30552b9d0355adf5f3fd5e3ea64b46
lcartercondon <liam.carter.condon@gmail.com>
2015-05-21 20:03:23 -0400

    initialized day two notes file

---
+++ 05-21-2015/src/DayTwo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author lcartercondon
+ *
+ */
+public class DayTwo {
+
+	/**
+	 * @param none
+	 * Day two of class
+	 * Talking about basic types, casting, basic operators
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
